6fa29b98bed7303e2a2cbdaf29f747491b9d6302
nawojmonika <nawojmonika@gmail.com>
2020-11-08 23:42:49 +0100

    Added beginning of the report description

--- part2.m
+++ part2.m
@@ -59,5 +59,36 @@ function ea = fe(T, H, ai)
     for i=1:length(n)
         sum = sum + (H(i) - fh(T, H, a, T(i)))^2;
     end
-    ea = sqrt(sum/(n+1))
+    ea = sqrt(sum/(n+1));
+end
+
+function y=interpolacja_funkcjami_sklejanymi(xp, yp, x)
+    if (x < xp(1))
+        y=xp(1);
+        return;
+    end
+    
+    if (x > xp(end))
+        y=xp(end);
+        return;
+    end
+    
+    for i = 1:length(xp) - 1
+        if x >= xp(i) && x <= xp(i + 1)
+            break;
+        end
+    end
+
+    A = [
+         1 xp(i)
+         1 xp(i + 1)
+        ]
+    b = [
+        yp(i)
+        yp(i + 1)
+    ]
+
+    a = A\b;
+
+    y = a(1) + a(2) * x;
 end
