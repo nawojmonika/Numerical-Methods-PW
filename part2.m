ad065d2883dbca9725b4da5c39e3707a237844d3
nawojmonika <nawojmonika@gmail.com>
2020-11-08 16:34:22 +0100

    Added counting errors for approximation

--- part2.m
+++ part2.m
@@ -1,18 +1,27 @@
 function part2
     T = [-1500, -1000, -300, -50, -1, 1, 20, 50, 200, 400, 1000, 2000];
-    h = [178, 176, 168, 161, 160, 160, 160.2, 161, 165, 168, 174, 179];
+    H = [178, 176, 168, 161, 160, 160, 160.2, 161, 165, 168, 174, 179];
     
     ti = -1500:1:2000;
     hi = zeros(length(ti), 1);
+    pi = 2:1:5;
+    ea = zeros(length(pi), 1);
     
-    ah = fa(T, h, 3);
+    for i=1:length(pi)
+        ea(i) = fe(T, H, pi(i));
+    end
     
-    for i=1:length(ti)
-        hi(i) = fh(T, h, ah, ti(i));
+    ea
+    p = find(ea==min(ea))
+    
+    ah = fa(T, H, p);
+    
+    for j=1:length(ti)
+        hi(j) = fh(T, H, ah, ti(j));
     end
   
 
-    plot(T, h, 'o', ti, hi);
+    plot(T, H, 'o', ti, hi);
 end
 
 function ah=fa(xT, yh, p)
@@ -26,7 +35,6 @@ function ah=fa(xT, yh, p)
             M(i, j+1) = xT(i)^j;
         end
     end
-    
     ah = (transpose(M)*M) \ (transpose(M)*Y);
 end
 
@@ -43,4 +51,14 @@ function vh = fh(T, h, ah, iT)
     for i=1:length(ah)
         vh = vh + ah(i) * iT^(i -1);
     end
+end
+
+function ea = fe(T, H, ai)
+    a = fa(T, H, ai);
+    n = length(T);
+    sum = 0;
+    for i=1:length(n)
+        sum = sum + (H(i) - fh(T, H, a, T(i)))^2;
+    end
+    ea = sqrt(sum/(n+1))
 end
