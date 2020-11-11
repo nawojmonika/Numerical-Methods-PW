ce4acdfd00e40a2600c2be7c15b217358b5fc6d8
nawojmonika <nawojmonika@gmail.com>
2020-11-11 14:27:56 +0100

    made conduction factor a matrix

--- src/part1.m
+++ src/part1.m
@@ -3,7 +3,7 @@ function part1
            25 ];
      step = 0.1;
      x = 0:step:5;
-     conduct = 160; % [J / s * m^2]
+     conduct(1:length(x)) = 160; % [J / s * m^2]
 
     ye = euler(x, y, step, conduct);
     yie = improvedEuler(x, y, step, conduct);
@@ -14,7 +14,7 @@ end
 
 function yie = euler(t, y, step, conduct)
     for i = 1:length(t)-1
-            y(:, i+1) = y(:, i) + step * tempModel(y(:,i), conduct);
+            y(:, i+1) = y(:, i) + step * tempModel(y(:,i), conduct(i));
     end
     yie = y;
 end
