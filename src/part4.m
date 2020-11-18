a24ad27907b04d11de9ae9a8bb41a370b2248dca
nawojmonika <nawojmonika@gmail.com>
2020-11-18 20:33:34 +0100

    scelethon for part 4

---
+++ src/part4.m
@@ -0,0 +1,13 @@
+function part4
+bar_num = 100 000;
+time = 24 * 60 * 60; % [s]
+container_change_t = 30; % [s]
+bar_change_t = 5; % [s]
+m_b = 0.25; % [kg]
+container_cool_t = m_b * 0.15 * 60 * 60; %[s]
+
+for i=1:bar_num
+
+end
+
+end
