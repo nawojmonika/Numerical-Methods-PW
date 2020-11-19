0008891e2ea9df36d7b0a027d789cf95649661b6
nawojmonika <nawojmonika@gmail.com>
2020-11-19 01:33:31 +0100

    Added most of the part 1 to the report

--- src/part1.m
+++ src/part1.m
@@ -1,19 +1,32 @@
 function part1
-     y = [ 1200
-           25 ]; % [C]
-     A = 0.0109; % [m^2]
-     mb = 0.2; % [kg]
-     mw = 2.5; % [kg]
-     cb = 3.85; % [J / kg * K]
-     cw = 4.1813; % [J /kg * K]
-     step = 0.1;
-     x = 0:step:5;
-     conduct(1:length(x)) = 160; % [J / s * m^2]
+    chart_size=[10 10 800 600];
+    Tb = [1200, 800, 1100, 1200, 800, 1100, 1100, 1100, 1100, 1100];
+    Tw = [25, 25, 70, 25, 25, 70, 70, 70, 70, 70];
+    mw = [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 5, 10, 2.5, 2.5]; % [kg]
+    t = [3, 3, 3, 5, 5, 2, 2, 2, 4, 5];
+    A = 0.0109; % [m^2]
+    mb = 0.2; % [kg]
+    cb = 3.85; % [J / kg * K]
+    cw = 4.1813; % [J /kg * K]
+    step = 0.1;
 
-    ye = euler(x, y, step, conduct, A, mb, mw, cb, cw);
-    yie = improvedEuler(x, y, step, conduct, A, mb, mw, cb, cw);
+    for i=1:length(Tb)
+        y = [ Tb(i)
+               Tw(i) ]; % [C]
+        x = 0:step:t(i);
+        h(1:length(x)) = 160; % [J / s * m^2]
+        ye = euler(x, y, step, h, A, mb, mw(i), cb, cw);
+        yie = improvedEuler(x, y, step, h, A, mb, mw(i), cb, cw);
 
-    plot(x, ye(1, :), x, ye(2, :), x, yie(1, :), x, yie(2, :));
+        fig=figure('Renderer', 'painters', 'Position', chart_size);
+        plot(x, ye(1, :), x, ye(2, :), x, yie(1, :), x, yie(2, :));
+        title('Przebiegi czasowe temperatury preta oraz oleju chlodzacego');
+        xlabel('t [s]');
+        ylabel(['T [' char(176) 'C]']);
+        legend('T_b obliczona funkcja Eulera', 'T_w obliczona funkcja Eulera', 'T_b obliczona  ulepszona funkcja Eulera','T_w obliczona ulepszona funkcja Eulera');
+        saveas(fig,sprintf('../assets/przebieg-czasowy-%d', i), 'png');
+        close;
+    end
 end
 
 function yie = euler(t, y, step, conduct, A, mb, mw, cb, cw)
