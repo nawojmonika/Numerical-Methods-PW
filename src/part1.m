fde246a5f21971a8c3c9d572bd3e5227ea6cfb48
nawojmonika <nawojmonika@gmail.com>
2020-11-19 21:51:18 +0100

    added calculating stepH to part1 program

--- src/part1.m
+++ src/part1.m
@@ -8,15 +8,34 @@ function part1
     mb = 0.2; % [kg]
     cb = 3.85; % [J / kg * K]
     cw = 4.1813; % [J /kg * K]
-    step = 0.1;
+    stepH = [0.001, 0.01, 0.1, 1];
 
-    for i=1:length(Tb)
-        y = [ Tb(i)
-               Tw(i) ]; % [C]
-        x = 0:step:t(i);
+    for i=1:length(stepH)
+            y = [ Tb(1)
+                   Tw(1) ]; % [C]
+            x = 0:stepH((i)):t(1);
+            h(1:length(x)) = 160; % [J / s * m^2]
+            ye = euler(x, y, stepH(i), h, A, mb, mw(1), cb, cw);
+            yie = improvedEuler(x, y, stepH(i), h, A, mb, mw(1), cb, cw);
+
+            fig=figure('Renderer', 'painters', 'Position', chart_size);
+            plot(x, ye(1, :), x, ye(2, :), x, yie(1, :), x, yie(2, :));
+            title('Przebiegi czasowe temperatury preta oraz oleju chlodzacego');
+            xlabel('t [s]');
+            ylabel(['T [' char(176) 'C]']);
+            legend('T_b obliczona funkcja Eulera', 'T_w obliczona funkcja Eulera', 'T_b obliczona  ulepszona funkcja Eulera','T_w obliczona ulepszona funkcja Eulera');
+            saveas(fig,sprintf('../assets/part1/krok-h-%d', i), 'png');
+            close;
+    end
+
+    for j=1:length(Tb)
+        step = 0.1;
+        y = [ Tb(j)
+               Tw(j) ]; % [C]
+        x = 0:step:t(j);
         h(1:length(x)) = 160; % [J / s * m^2]
-        ye = euler(x, y, step, h, A, mb, mw(i), cb, cw);
-        yie = improvedEuler(x, y, step, h, A, mb, mw(i), cb, cw);
+        ye = euler(x, y, step, h, A, mb, mw(j), cb, cw);
+        yie = improvedEuler(x, y, step, h, A, mb, mw(j), cb, cw);
 
         fig=figure('Renderer', 'painters', 'Position', chart_size);
         plot(x, ye(1, :), x, ye(2, :), x, yie(1, :), x, yie(2, :));
@@ -24,7 +43,9 @@ function part1
         xlabel('t [s]');
         ylabel(['T [' char(176) 'C]']);
         legend('T_b obliczona funkcja Eulera', 'T_w obliczona funkcja Eulera', 'T_b obliczona  ulepszona funkcja Eulera','T_w obliczona ulepszona funkcja Eulera');
-        saveas(fig,sprintf('../assets/przebieg-czasowy-%d', i), 'png');
+        saveas(fig,sprintf('../assets/part1/przebieg-czasowy-%d', j), 'png');
+        end_values = [ye(1, end), ye(2, end), yie(1, end), yie(2, end)];
+        writematrix(end_values,'../assets/part1/przebieg-czasowy.txt','WriteMode','append')
         close;
     end
 end
